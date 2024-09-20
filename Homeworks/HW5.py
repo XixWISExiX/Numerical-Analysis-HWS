72f35897f6f67cd030ea0617366196fb71af30e0
Joshua Wiseman <addalooptoit@gmail.com>
2024-09-19 22:29:28 -0500

    HW5 complete

--- Homeworks/HW5.py
+++ Homeworks/HW5.py
@@ -48,7 +48,9 @@ def better_bisection(f, a, b, max_iter, tolerance):
     """
     Approximates a root p in [a,b] of the function f with error smaller than 
     a given tolerance.
-    Note that f(a)f(b) has to be negative for this method to work
+    Note that f(a)f(b) has to be negative for this method to work & the function
+    needs to be continuous. There is no continuous check in this algorithm so
+    you will need to do that yourself.
     :param f: function whose root is approximated
     :param a: left bound on the position of the root
     :param b: right bond on the position of the root
@@ -102,7 +104,7 @@ def f(x):
 
 a = 0
 b = 1
-max_iter = 40
+max_iter = 1000
 tolerance = 10 ** (-9)
 
 print("C1 Testing")
@@ -129,8 +131,8 @@ def f(x):
 
 a = 0
 b = 1
-max_iter = 40
-tolerance = 10 ** (-5) # 5 decimal places
+max_iter = 1000
+tolerance = 10 ** (-9)
 
 print("\nC2 Results")
 print("Bisection algo given",bisection(f, a, b, max_iter, tolerance))
@@ -152,9 +154,9 @@ def f(x):
 a = 0
 b = 1
 max_iter = 1000
-tolerance = 10 ** (-5) # 5 decimal places
+tolerance = 10 ** (-9)
 
-print("\nC2 Results")
+print("\nC3 Results")
 print("Bisection algo given",bisection(f, a, b, max_iter, tolerance))
 print("C1 Bisection algo", better_bisection(f, a, b, max_iter, tolerance))
 
@@ -169,14 +171,169 @@ def f(x):
     :param x: The input variable for the equation
     :return: The function result 
     '''
-    # x**3 - 2*x + 1
-    # and
-    # x
-    return 
+    # y = x**3 - 2*x + 1 and y = x
+    # x = x**3 - 2*x + 1 
+    # 0 = x**3 - 3*x + 1 
+    return x**3 - 3*x + 1
 
+a = 0
+b = 1
 max_iter = 1000
-tolerance = 10 ** (-5) # 5 decimal places
+tolerance = 10 ** (-9)
 
-print("\nC2 Results")
+print("\nC4 Results")
 print("Bisection algo given",bisection(f, a, b, max_iter, tolerance))
 print("C1 Bisection algo", better_bisection(f, a, b, max_iter, tolerance))
+
+
+########################################################################################
+# C5
+########################################################################################
+
+def f(x):
+    '''
+    A function f that takes in a singular input x 
+    :param x: The input variable for the equation
+    :return: The function result 
+    '''
+    return np.tan(x) - x
+
+a = 4
+b = 5
+max_iter = 1000
+tolerance = 10 ** (-9)
+
+print("\nC5 Results")
+print("(a) Bisection algo given",bisection(f, a, b, max_iter, tolerance))
+print("(a) C1 Bisection algo", better_bisection(f, a, b, max_iter, tolerance))
+
+a = 1
+b = 2
+
+print("(b) Bisection algo given",bisection(f, a, b, max_iter, tolerance))
+print("(b) C1 Bisection algo", better_bisection(f, a, b, max_iter, tolerance))
+
+print("Explaination: In the equation tan(x) - x, the algorithm gives a root in the interval [1,2].")
+print("However, this is actually not the case. The value returned was 1.5707 or pi/2 from the algorithm.")
+print("But when you plug that into tan(x) you get undefined.")
+print("This is because tan(x) is not a continuous function and therefore violates the other condition")
+print("for the algorithm to work, that being the function must be continuous.")
+
+########################################################################################
+# C6
+########################################################################################
+
+def f(x):
+    '''
+    A function f that takes in a singular input x 
+    :param x: The input variable for the equation
+    :return: The function result 
+    '''
+    # x = 3**0.5
+    # x**2 = 3
+    # x**2 - 3 = 0
+    return x**2 - 3
+
+a = 1
+b = 2
+max_iter = 1000
+tolerance = 10 ** (-9)
+
+print("\nC6 Results")
+print("Bisection algo given",bisection(f, a, b, max_iter, tolerance))
+print("C1 Bisection algo", better_bisection(f, a, b, max_iter, tolerance))
+
+########################################################################################
+# C7
+########################################################################################
+
+# Algebra
+# y = ux + v
+# u = rise/run = [f(b)-(fa)]/[b-a]
+# so
+# y = u(x-a) move to the right by a
+# y = u(x-a) + f(a) up by f(a)
+# y = {[f(b)-(fa)]/[b-a]}(x-a) + f(a)
+# ^ the equation of the secant line
+# 0 = {[f(b)-(fa)]/[b-a]}(x-a) + f(a)
+# -f(a) = {[f(b)-(fa)]/[b-a]}(x-a)
+# -f(a)/{[f(b)-(fa)]/[b-a]} = x-a
+# (-f(a)/{[f(b)-(fa)]/[b-a]}) + a = x
+# (-f(a)/{[f(b)-(fa)]/[b-a]}) + a = x
+# x = (a*fa - b*fa)/(fb-fa) + a
+# p1 = x = [a*f(b) - b*f(a)] / [f(b) - f(a)]
+
+def false_position(f, a, b, max_iter, tolerance):
+    """
+    Approximates a root p in [a,b] of the function f with error smaller than 
+    a given tolerance.
+    Note that f(a)f(b) has to be negative for this method to work & the function
+    needs to be continuous. There is no continuous check in this algorithm so
+    you will need to do that yourself.
+    :param f: function whose root is approximated
+    :param a: left bound on the position of the root
+    :param b: right bond on the position of the root
+    :param max_iter: maximum number of iteration to reach the tolerance
+    :param tolerance: maximal allowed error
+
+    :return: converged - flag indicating if the method reached the precision
+             root - approximation of the root
+    """
+
+    fa = f(a)
+    fb = f(b)
+
+    # If the signs aren't the same, the condition doesn't match to run this algo (saves from underflow)
+    if np.sign(fa) == np.sign(fb):
+        return False, 0
+
+    p_minus_one = 0 
+    for _ in np.arange(max_iter):
+        p = (a*fb - b*fa)/(fb-fa)
+        fp = f(p)
+
+        if fp == 0 or abs(p-p_minus_one) < tolerance:
+            return True, p
+
+        if np.sign(fa) == np.sign(fp):
+            a = p
+            fa = fp
+        else:
+            b = p
+            fb = fp
+        p_minus_one = p
+
+    return False, 0
+
+
+
+def f(x):
+    '''
+    A function f that takes in a singular input x 
+    :param x: The input variable for the equation
+    :return: The function result 
+    '''
+    return x + 0.9
+
+a = -1
+b = 1
+max_iter = 1000
+tolerance = 10 ** (-9) # 9 decimal places
+
+print("\nC7 Results")
+print("(a) False Position Algorithm", false_position(f, a, b, max_iter, tolerance))
+print("(a) Bisection Algorithm", better_bisection(f, a, b, max_iter, tolerance))
+
+def f(x):
+    '''
+    A function f that takes in a singular input x 
+    :param x: The input variable for the equation
+    :return: The function result 
+    '''
+    return x**2 - 1.8*x + 0.71
+
+a = -1
+b = 1
+
+print("(b) False Position Algorithm", false_position(f, a, b, max_iter, tolerance))
+print("(b) Bisection Algorithm", better_bisection(f, a, b, max_iter, tolerance))
